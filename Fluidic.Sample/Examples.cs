fb42a6d94915456768c9a10fba17f617f49e130c
bmazzarol-bunnings <bmazzarol@bunnings.com.au>
2025-04-12 13:58:24 +0800

    feat: use new MTP

--- Fluidic.Sample/Examples.cs
+++ Fluidic.Sample/Examples.cs
@@ -6,4 +6,7 @@ public static partial class Examples
 {
     [StringTemplate("This is a very simple example")]
     public static partial void Test(this StringBuilder builder);
+
+    [FileTemplate]
+    public static partial void FileTest(this StringBuilder builder);
 }
